e2e89a4c5bcd583676730ac6668446028965cb3c
Ping1495 <ping15512@gmail.com>
2024-06-03 04:31:51 +0700

    add:bmi calculate

--- src/components/bmi.jsx
+++ src/components/bmi.jsx
@@ -1,6 +1,40 @@
 import Header from "./header";
+import { useState, useEffect } from "react";
 
 function BMI() {
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+  const [bmi, setBmi] = useState("?");
+  const [message, setMessage] = useState("");
+  const [bestMinWeight, setBestMinWeight] = useState("");
+  const [bestMaxWeight, setBestMaxWeight] = useState("");
+
+  const calculateBMI = () => {
+    //bmi calculate
+    const heightInMeters = height / 100;
+    const bmiValue = (weight / (heightInMeters * heightInMeters)).toFixed(2);
+    setBmi(bmiValue);
+
+    //message
+    if (bmiValue < 18.5) {
+      setMessage("Underweight");
+    } else if (bmiValue >= 18.5 && bmiValue <= 24.99) {
+      setMessage("Normal weight");
+    } else if (bmiValue >= 25 && bmiValue <= 29.99) {
+      setMessage("Overweight");
+    } else {
+      setMessage("Obese");
+    }
+
+    //best weight
+    //min
+    const minWeight = (18.5 * (heightInMeters * heightInMeters)).toFixed(2);
+    setBestMinWeight(minWeight);
+    //max
+    const maxWeight = (24.9 * (heightInMeters * heightInMeters)).toFixed(2);
+    setBestMaxWeight(maxWeight);
+  };
+
   return (
     <>
       <div className="bg-blue-500 h-screen">
@@ -13,47 +47,56 @@ function BMI() {
             <form>
               <div>
                 <label
-                  for="height"
-                  class="block text-lg font-normal text-[#0A3288] mb-2 mt-4"
+                  htmlFor="height"
+                  className="block text-lg font-normal text-[#0A3288] mb-2 mt-4"
                 >
                   Height (cm)
                 </label>
                 <input
+                  value={height}
+                  onChange={(e) => setHeight(e.target.value)}
                   type="text"
                   id="height"
                   name="height"
-                  class="mt-1 p-2 border-2 border-[#0A3288] rounded w-[400px]"
+                  className="mt-1 p-2 border-2 border-[#0A3288] rounded w-[400px]"
                 />
               </div>
               <div>
                 <label
-                  for="weight"
-                  class="block text-lg font-normal text-[#0A3288] mb-2 mt-4"
+                  htmlFor="weight"
+                  className="block text-lg font-normal text-[#0A3288] mb-2 mt-4"
                 >
                   Weight (kg)
                 </label>
                 <input
+                  value={weight}
+                  onChange={(e) => setWeight(e.target.value)}
                   type="text"
                   id="weight"
                   name="weight"
-                  class="mt-1 p-2 border-2 border-[#0A3288] rounded w-[400px]"
+                  className="mt-1 p-2 border-2 border-[#0A3288] rounded w-[400px]"
                 />
               </div>
             </form>
             <div>
-              <button className="w-[250px] bg-[#0A3288] text-white text-[20px] font-semibold border border-white py-2 px-6 rounded-xl mb-[20px] mt-[30px]">
+              <button
+                className="w-[250px] bg-[#0A3288] text-white text-[20px] font-semibold border border-white py-2 px-6 rounded-xl mb-[20px] mt-[30px]"
+                onClick={calculateBMI}
+              >
                 calculate your BMI
               </button>
             </div>
-            <div className="text-[#0A3288] mb-[20px] flex flex-col justify-center items-center">
+            <div className="text-[#0A3288] mb-[15px] flex flex-col justify-center items-center">
               <p className="text-xl font-midium ">Your BMI is</p>
               <p className="text-3xl font-midium  py-2 px-2 mt-2 border-2 border-blue-500 rounded-xl">
-                22.0
+                {bmi}
               </p>
             </div>
+
             <div>
-              <p className="text-[#0A3288] mt-2">
-                You are NORMAL & Your best weight is 51-64 kg
+              <p className="text-[#0A3288] text-center">
+                You are a {message} <br /> Your best weight is {bestMinWeight}-
+                {bestMaxWeight} kg
               </p>
             </div>
           </div>
